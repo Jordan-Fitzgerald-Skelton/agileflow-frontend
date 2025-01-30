a03004e24cafb8284a3d924f6c6a3e49c9003603
Your Name <jordanfitzskelton45@gmail.com>
2025-01-30 18:02:12 +0000

    fixing Auth0 login

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
 <Auth0Provider
-    domain={process.env.Auth_Domain}
-    clientId={process.env.Auth_Client_ID}
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_uri: window.location.origin
     }}
   >
     <App />
-</Auth0Provider>,
+</Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
